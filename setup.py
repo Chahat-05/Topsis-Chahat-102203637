6c250b5d1251cf89b5bf5539cc11cd453d9585f0
Chahat Verma <126184686+Chahat-05@users.noreply.github.com>
2025-01-19 19:22:30 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,6 +13,8 @@ setup(
             'topsis-cli=topsis.Chahat_102203637:main',  # This will be the entry point for your CLI
         ],
     },
+    
+
     description='A Python package to implement the TOPSIS method for multi-criteria decision analysis.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
@@ -20,4 +22,10 @@ setup(
     author_email='cverma_be22@thapar.edu',
     url='https://github.com/Chahat-05/Topsis-Chahat-102203637.git',  # Replace with your repo URL
     license='MIT',
+    classifiers=[
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',  # You can change this if you use a different license
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3 :: Only',  # Specifies this package only supports Python 3
+],
 )
